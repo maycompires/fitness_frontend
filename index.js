3977d50f1c3681484ace2e388c4a8e7afc5646c7
maycompires <maycompires@hotmail.com>
2024-02-13 21:27:02 -0300

    first comit

---
+++ index.js
@@ -0,0 +1,8 @@
+let nome = window.prompt('Por Favor digite seu nome: ')
+    alert("Obrigado agora clique em Exibir")
+
+function exibirMenssagem(){
+    console.log(`Olá ${nome} Seja Bem vindo!`)
+}
+
+exibirMenssagem()
